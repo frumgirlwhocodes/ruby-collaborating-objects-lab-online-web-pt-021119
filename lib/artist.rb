b659153423ed20fd26209da1b52003ea9da1463b
lieby weintraub <liebahinda@gmail.com>
2019-03-20 00:28:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,16 +24,39 @@ class Artist
   end 
   def self.find_or_create_by_name(name)
     if self.all.detect{ |artist| artist.name == name}
+<<<<<<< HEAD
       @@all.detect{ |artist| artist.name == name}  
       #self.all.detect{ |artist| artist.name == name}
        else 
     artist= self.new(name)
     artist.save
     artist 
+=======
+     @all.detect{ |artist| artist.name == name}
+    else 
+     artist = Artist.new(name)
+    artist.save
+   artist
+>>>>>>> 73dbe774819d3ad0fd10af113537bcc545438e78
     end 
-    
   end 
- # binding.pry 
+   # def self.find_by_name(name)
+ # self.all.find {|s| s.name == name}
+ # end 
+ 
+ #def self.create_by_name(name)
+  # artist = Artist.new(name)
+  #  artist.save
+  #  artist
+# end
+
+# def self.find_or_create_by_name(name)
+ #  if self.find_by_name(name)
+ #    self.find_by_name(name)
+ #    else self.create_by_name(name)
+ #    end
+# end
+    
   def print_songs
     @songs.collect do |song|
       puts song.name
