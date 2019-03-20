4cdc587b1ac30e606623883c38f8ccfd6665fc7c
lieby weintraub <liebahinda@gmail.com>
2019-03-20 00:33:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,16 @@
 class Song 
-<<<<<<< HEAD
+
   attr_accessor :name, :artist
-=======
+
   attr_accessor :name, :artist, :genre
->>>>>>> 73dbe774819d3ad0fd10af113537bcc545438e78
+
 
    def initialize(name)
     @name=name 
     @artist=artist
-  end
+end 
 
-<<<<<<< HEAD
+
     def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
@@ -23,16 +23,7 @@ class Song
     song
   end
 end 
-=======
-    def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
-  end
 
-   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
->>>>>>> 73dbe774819d3ad0fd10af113537bcc545438e78
+   
+
+  
